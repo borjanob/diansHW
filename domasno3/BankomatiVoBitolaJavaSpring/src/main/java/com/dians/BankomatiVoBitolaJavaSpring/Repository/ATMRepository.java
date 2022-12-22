35b72c6dc5c35f5a4b380e32dc871609fe52e435
Ivica Cickoski <icickovski@gmail.com>
2022-12-22 20:12:28 +0100

    Revert "starting features for backend"
    This reverts commit 2c42869fc76953a93af883d1308e04d9442399b7.

--- domasno3/BankomatiVoBitolaJavaSpring/src/main/java/com/dians/BankomatiVoBitolaJavaSpring/Repository/ATMRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dians.BankomatiVoBitolaJavaSpring.Repository;
-
-import com.dians.BankomatiVoBitolaJavaSpring.Entity.ATM;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ATMRepository extends JpaRepository<ATM, Long> {
-}
